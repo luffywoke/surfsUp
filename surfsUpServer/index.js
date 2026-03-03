9fd91c4cd79588d6c16a280239694611db919271
gabeRam <gaberam310@gmail.com>
2026-03-03 08:58:28 -0800

    I think just deleted a line

--- surfsUpServer/index.js
+++ surfsUpServer/index.js
@@ -10,11 +10,11 @@ const PORT = 5000;
 // Adding middleware to read requests that have JSON data 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    
-})
+//Connect mongoose to MongoDB
+mongoose.connect(process.env.MONGO_URI,)
 .then(() => console.log('MongoDB connected successfully'))
 .catch((err) => console.error('MongoDB connection error:', err));
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
