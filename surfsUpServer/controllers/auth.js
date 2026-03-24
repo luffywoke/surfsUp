adb127d78c37ecbb2a2e2f48cbc74822baf0e1c7
gabeRam <gaberam310@gmail.com>
2026-03-24 15:30:27 -0700

    Added more to registerUser function to check if email exists and bcrypt password

--- surfsUpServer/controllers/auth.js
+++ surfsUpServer/controllers/auth.js
@@ -1,7 +1,20 @@
-function registerUser (req, res) {
-
+async function registerUser (req, res) {
+    try {
+        const {username, password, email} = req.body;
+        // Check if email exists
+        const emailExists = await User.findOne({email: email});
+        if (emailExists) {
+            return res.status(409).json({message: 'Email already exists'});
+        }
+        // Hashing password with bcrypt
+        const hashedPassword = await bcrypt.hash(password, 10);
+    }
+    catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).json({message: 'Server error'});
+    }
 }
 
-function loginUser (req, res) {
+async function loginUser (req, res) {
 
 }
