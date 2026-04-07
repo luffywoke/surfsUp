ab9f59daa49e14f92158536848ca0f0a16b4a018
gabeRam <gaberam310@gmail.com>
2026-04-07 07:37:55 -0700

    Setting up routes to diff pages

--- surfsUp-client/src/main.jsx
+++ surfsUp-client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// BrowserRouter allows us to navigate through different pages
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
+);
