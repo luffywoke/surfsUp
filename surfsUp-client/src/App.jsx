1d53c16ecbce003826b5f7b36825e9ed5cebcc74
gabeRam <gaberam310@gmail.com>
2026-04-07 08:31:43 -0700

    Adding more react router and pages

--- surfsUp-client/src/App.jsx
+++ surfsUp-client/src/App.jsx
@@ -1,11 +1,17 @@
-
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
     return (
-        <div>
-            <h1>Welcome to SurfsUp!</h1>
-        </div>
-    );
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} /> 
+        </Routes>
+    )
 }
 
 export default App;
