4d34e5082946cef22051a97c53d9da43f797be38
gabeRam <gaberam310@gmail.com>
2026-04-08 08:01:46 -0700

    added login form

--- surfsUp-client/src/pages/Login.jsx
+++ surfsUp-client/src/pages/Login.jsx
@@ -1,7 +1,49 @@
+import { useState } from 'react';
+
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        // Prevents page from refreshing on submit like regular HTML forms
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:5000/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                // Send the form data here as JSON
+                body: JSON.stringify({email, password})
+            });
+
+            const data = await response.json();
+            
+        }
+        catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
     return (
         <div>
             <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type='email'
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input 
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type='submit'>Submit</button>
+            </form>
         </div>
     )
 }
